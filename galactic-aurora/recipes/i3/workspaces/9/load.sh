17a304b1b98abcdc53501fa169a6e38135dca792
Anomalyce <47085833+anomalyce@users.noreply.github.com>
2020-08-23 15:09:26 +0200

    Changed bitwarden dpi scale ratio

--- galactic-aurora/recipes/i3/workspaces/9/load.sh
+++ galactic-aurora/recipes/i3/workspaces/9/load.sh
@@ -7,4 +7,4 @@ i3-msg --quiet "exec --no-startup-id nohup electron-mail --high-dpi-support=1 --
 i3-msg --quiet "exec --no-startup-id nohup todoist --high-dpi-support=1 --force-device-scale-factor=1.5 >/dev/null 2>&1 &"
 
 # Bitwarden
-i3-msg --quiet "exec --no-startup-id nohup bitwarden --high-dpi-support=1 --force-device-scale-factor=1.1 >/dev/null 2>&1 &"
+i3-msg --quiet "exec --no-startup-id nohup bitwarden --high-dpi-support=1 --force-device-scale-factor=1.5 >/dev/null 2>&1 &"
