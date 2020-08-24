f65c16a8d9262ba9e1588d4625a69b5826fa6bb1
Anomalyce <47085833+anomalyce@users.noreply.github.com>
2020-08-24 10:48:52 +0200

    Updated i3 picom loading

--- galactic-aurora/recipes/i3/workspaces/global/load.sh
+++ galactic-aurora/recipes/i3/workspaces/global/load.sh
@@ -6,4 +6,4 @@ export CONFIGPATH="${HOME}/.config"
 i3-msg --quiet "exec --no-startup-id nohup dunst >/dev/null 2>&1 &"
 
 # Compton
-i3-msg --quiet "exec --no-startup-id nohup dbus-launch picom --config=\"${CONFIGPATH}/picom/picom.conf\" >/dev/null 2>&1 &"
+i3-msg --quiet "exec --no-startup-id nohup dbus-launch picom --config=${CONFIGPATH}/picom/picom.conf >/dev/null 2>&1 &"
