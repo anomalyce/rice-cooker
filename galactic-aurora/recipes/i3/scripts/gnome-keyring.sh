f0519e5c93804d350c114172b633c3377f5ff08f
Anomalyce <47085833+anomalyce@users.noreply.github.com>
2021-01-23 23:34:39 +0100

    Auto login keyring

---
+++ galactic-aurora/recipes/i3/scripts/gnome-keyring.sh
@@ -0,0 +1,7 @@
+eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
+
+export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
+
+sleep 2
+
+eval $(/usr/bin/gnome-keyring-daemon -r -d --unlock <<<"")
