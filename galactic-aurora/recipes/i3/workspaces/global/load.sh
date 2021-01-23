a8d58d2b12544d31d66b463661874198af880728
Anomalyce <47085833+anomalyce@users.noreply.github.com>
2021-01-23 22:00:30 +0100

    Changes

--- galactic-aurora/recipes/i3/workspaces/global/load.sh
+++ galactic-aurora/recipes/i3/workspaces/global/load.sh
@@ -6,7 +6,7 @@ export CONFIGPATH="${HOME}/.config"
 i3-msg --quiet "exec --no-startup-id nohup dunst >/dev/null 2>&1 &"
 
 # Compton
-i3-msg --quiet "exec --no-startup-id nohup dbus-launch picom --transparent-clipping --experimental-backends --config=${CONFIGPATH}/picom/picom.conf >/dev/null 2>&1 &"
+i3-msg --quiet "exec --no-startup-id nohup dbus-launch picom --experimental-backends --config=${CONFIGPATH}/picom/picom.conf >/dev/null 2>&1 &"
 
 # KeeWeb (Scratchpad)
 #i3-msg --quiet "exec --no-startup-id nohup keeweb --force-device-scale-factor=2 >/dev/null 2>&1 &"
