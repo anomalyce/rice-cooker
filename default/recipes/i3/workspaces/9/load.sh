a3b360e142ffcb18412b7c8d26bb02a1124efa73
Anomalyce <47085833+anomalyce@users.noreply.github.com>
2021-03-25 20:56:33 +0100

    Adjustments

--- default/recipes/i3/workspaces/9/load.sh
+++ default/recipes/i3/workspaces/9/load.sh
@@ -10,7 +10,9 @@ i3-msg --quiet "exec --no-startup-id nohup notion-app >/dev/null 2>&1 &"
 #i3-msg --quiet "exec --no-startup-id QT_SCALE_FACTOR=1.5 nohup TogglDesktop >/dev/null 2>&1 &"
 
 # Bitwarden
-i3-msg --quiet "exec --no-startup-id nohup bitwarden --high-dpi-support=1 --force-device-scale-factor=1.05 >/dev/null 2>&1 &"
+# i3-msg --quiet "exec --no-startup-id nohup bitwarden --high-dpi-support=1 --force-device-scale-factor=1.05 >/dev/null 2>&1 &"
+i3-msg --quiet "exec --no-startup-id nohup bitwarden --high-dpi-support=1 >/dev/null 2>&1 &"
 
 # ProtonMail
-i3-msg --quiet "exec --no-startup-id nohup electron-mail --high-dpi-support=1 --force-device-scale-factor=1.5 >/dev/null 2>&1 &"
+# i3-msg --quiet "exec --no-startup-id nohup electron-mail --high-dpi-support=1 --force-device-scale-factor=1.5 >/dev/null 2>&1 &"
+i3-msg --quiet "exec --no-startup-id nohup electron-mail --high-dpi-support=1 >/dev/null 2>&1 &"
